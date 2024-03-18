50c2f28833246ff4177974710db858cebbe39d35
Rafał Chomczyk <hello@rafal.moe>
2024-03-18 17:23:53 +0100

    Fix code style inconsistencies; Add modified version of iridium colors, which could be used in the future instead of minimessage

---
+++ auroramc-network-shared/auroramc-network-shared-server/auroramc-iridium/src/main/java/com/iridium/iridiumcolors/tag/GradientTagResolver.java
@@ -0,0 +1,47 @@
+package com.iridium.iridiumcolors.tag;
+
+import static com.iridium.iridiumcolors.IridiumColors.color;
+import static java.util.regex.Pattern.compile;
+
+import java.awt.Color;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Represents a gradient color pattern which can be applied to a String.
+ */
+public class GradientTagResolver implements TagResolver {
+
+  Pattern pattern = compile("<gradient:([0-9A-Fa-f]{6})>(.*?)</gradient:([0-9A-Fa-f]{6})>");
+
+  /**
+   * Applies a gradient pattern to the provided String. Output might me the same as the input if
+   * this pattern is not present.
+   *
+   * @param string The String to which this pattern should be applied to
+   * @return The new String with applied pattern
+   */
+  public String resolve(final String string) {
+    final Matcher matcher = pattern.matcher(string);
+    final StringBuilder result = new StringBuilder();
+    int lastMatchEnd = 0;
+    while (matcher.find()) {
+      final String start = matcher.group(1);
+      final String end = matcher.group(3);
+      final String content = matcher.group(2);
+      result
+          .append(string, lastMatchEnd, matcher.start())
+          .append(
+              color(
+                  content,
+                  new Color(Integer.parseInt(start, 16)),
+                  new Color(Integer.parseInt(end, 16))
+              )
+          );
+      lastMatchEnd = matcher.end();
+    }
+    return result
+        .append(string.substring(lastMatchEnd))
+        .toString();
+  }
+}
---
+++ auroramc-network-shared/auroramc-network-shared-server/auroramc-iridium/src/main/java/com/iridium/iridiumcolors/tag/StaticTagResolver.java
@@ -0,0 +1,38 @@
+package com.iridium.iridiumcolors.tag;
+
+import static com.iridium.iridiumcolors.IridiumColors.getColor;
+import static java.util.regex.Pattern.compile;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StaticTagResolver implements TagResolver {
+
+  Pattern pattern = compile("<static:([0-9A-Fa-f]{6})>|#\\{([0-9A-Fa-f]{6})}");
+
+  /**
+   * Applies a solid RGB color to the provided String. Output might me the same as the input if this
+   * pattern is not present.
+   *
+   * @param string The String to which this pattern should be applied to
+   * @return The new String with applied pattern
+   */
+  public String resolve(final String string) {
+    final Matcher matcher = pattern.matcher(string);
+    final StringBuilder result = new StringBuilder();
+    int lastMatchEnd = 0;
+    while (matcher.find()) {
+      String color = matcher.group(1);
+      if (color == null) {
+        color = matcher.group(2);
+      }
+      result
+          .append(string, lastMatchEnd, matcher.start())
+          .append(getColor(color));
+      lastMatchEnd = matcher.end();
+    }
+    return result
+        .append(string.substring(lastMatchEnd))
+        .toString();
+  }
+}
